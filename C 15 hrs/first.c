00924bb18b53e2138d7b3b7d95e45ee4bdad9c55
Sultan Mahir Muhtasim Arian <sultanmahirmuhtasimarian@Sultans-MacBook-Air.local>
2024-07-05 01:55:00 +0600

    linking uncle to mama

---
+++ C 15 hrs/first.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+int main() 
+
+{
+ float x,y, Div ;
+ 
+ printf("Enter two values:-\n") ;
+
+ scanf("%f %f", &x , &y ) ; 
+
+ Div= x/y ;
+
+printf( "The value of Div is:- %f ", Div);
+
+return 0;
+
+}
+
---
+++ C 15 hrs/iteration.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+main()
+
+{
+
+
+    int i=1 ; 
+
+    printf("First 6 terms of the geometric series with ratio 2 are:- \n") ;
+
+    printf( "%d \n", i) ;
+
+    i=i+i ;
+
+    printf( "%d \n", i) ;
+
+    i=i+i ;
+
+    printf( "%d \n", i) ;
+
+    i=i+i ;
+
+    printf( "%d \n", i) ;
+
+    i=i+i ;`
+
+    printf("%d\n",i) ;
+
+     i=i+i ;
+
+    printf("%d\n",i) ;
+
+
+
+    return 0; 
+
+
+}
